46ee804d3324d4a606291a4ef509afa8cdf0ae76
Clerick Barrion <clerickbarrion@gmail.com>
2024-08-17 03:13:02 -0400

    contract and intro

---
+++ scripts/headerfooter.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const header = document.querySelector('header')
+    header.innerHTML = `
+        <h1>Clerick Barrion's Cool Bear | WEB115</h1>
+        <nav>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a href="introduction.html">Introduction</a></li>
+                <li><a href="contract.html">Contract</a></li>
+            </ul>
+        </nav>
+    `
+    const footer = document.querySelector('footer')
+    footer.innerHTML = `
+        <ul>
+            <li><a href="https://github.com/cbarrio2-CPCC">GitHub</a></li>
+            <li><a href="https://cbarrio2-cpcc.github.io">GitHub.io</a></li>
+            <li><a href="https://cbarrio2-cpcc.github.io/web115">WEB115.io</a></li>
+            <li><a href="https://www.freecodecamp.org/clerick">freeCodeCamp</a></li>
+            <li><a href="https://www.codecademy.com/profiles/cbarrion">Codecademy</a></li>
+            <li><a href="https://jsfiddle.net/user/clerickbarrion">JSFiddle</a></li>
+            <li><a href="https://www.linkedin.com/in/clerickbarrion">LinkedIn</a></li>
+        </ul>
+        <br>
+        <p>Page built by CookieBear &copy;2024</p>
+        <a href="http://validator.w3.org/check?uri=${window.location.href}" style = "text-decoration: none">
+            <img src="images/validation_button_html-blue.png" alt="Validate HTML" />
+        </a>
+        <a href="http://jigsaw.w3.org/css-validator/validator?uri=${window.location.href}" style = "text-decoration: none">
+            <img src="images/validation_button_css-blue.png" alt="Validate CSS" />
+        </a>
+    `
+})
